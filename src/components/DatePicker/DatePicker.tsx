0617c9aa0803a899b97c732881003bf464260525
teenage1ove <kirillstars7@gmail.com>
2025-01-07 13:44:43 +0300

    Add datepicker

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -5,6 +5,7 @@ interface MovieDate {
     dayOfWeek: string;
     day: number;
     formattedDate: string; // Тип для форматированной даты, например, "2024-08-05"
+    date: Date
   }
 
 function DatePicker() {
@@ -12,25 +13,32 @@ function DatePicker() {
   const [startDate, setStartDate] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(new Date());
 
+
   useEffect(() => {
         setCurrentDate(new Date());
     }, []);
 
-    console.log(currentDate)
-    const generateDates = () => {
+  
+    function generateDates() {
         const days = [];
+
         for (let i = 0; i < 7; i++) {
-          const date = new Date();
+          const date = new Date(startDate);
             date.setDate(startDate.getDate() + i);
             const dayOfWeek = date.toLocaleDateString('ru-RU', { weekday: 'short' });
             const day = date.getDate();
             const formattedDate = date.toISOString().split('T')[0];
             days.push({ dayOfWeek, day, date, formattedDate });
         }
+
         return days;
-    };
+    }
+
+    const dates = generateDates();
+    console.log(dates)
 
     const handleDateClick = (date: Date) => {
+        console.log(date)
         setSelectedDate(date);
     }
 
@@ -46,6 +54,7 @@ function DatePicker() {
         setStartDate(prevDate => {
             const newDate = new Date(prevDate);
             newDate.setDate(prevDate.getDate() + 7);
+            console.log(newDate)
             return newDate
         });
     };
@@ -56,14 +65,18 @@ function DatePicker() {
 
   const isToday = (date: MovieDate) => {
     const today = new Date();
-    return date.formattedDate === today.toISOString().split('T')[0];
+    return (
+        date.date.getDate() === today.getDate() &&
+        date.date.getMonth() === today.getMonth() &&
+        date.date.getFullYear() === today.getFullYear()
+    );
 };
 
   return (
     <div className="date-picker">
       <div className="date-navigation">
       { currentDate < startDate && <button onClick={handlePrevClick}>&lt;</button>}
-        {generateDates().map((day) => (
+        {dates.map((day) => (
           <div
             key={day.formattedDate}
             className={`date-item ${isDateSelected(day) ? 'selected' : ''}`}
