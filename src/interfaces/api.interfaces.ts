b3a9358ca622e27602d5a2c4265b8576ae9437cc
teenage1ove <kirillstars7@gmail.com>
2025-01-27 22:21:59 +0300

    add interfaces

--- src/interfaces/api.interfaces.ts
+++ src/interfaces/api.interfaces.ts
@@ -1,37 +1,37 @@
 export interface RootInterface {
-    success: boolean;
-    result: ResultResponse;
-  }
-  
-  export interface ResultResponse {
-    halls: Hall[];
-    films: Film[];
-    seances: Seance[];
-  }
-  
-  export interface Seance {
-    id: number;
-    seance_hallid: number;
-    seance_filmid: number;
-    seance_time: string;
-  }
-  
-  export interface Film {
-    id: number;
-    film_name: string;
-    film_duration: number;
-    film_description: string;
-    film_origin: string;
-    film_poster: string;
-  }
-  
-  export interface Hall {
-    id: number;
-    hall_name: string;
-    hall_rows: number;
-    hall_places: number;
-    hall_config: string[][];
-    hall_price_standart: number;
-    hall_price_vip: number;
-    hall_open: number;
-  }
+	success: boolean
+	result: ResultResponse
+}
+
+export interface ResultResponse {
+	halls: Hall[]
+	films: Film[]
+	seances: Seance[]
+}
+
+export interface Seance {
+	id: number
+	seance_hallid: number
+	seance_filmid: number
+	seance_time: string
+}
+
+export interface Film {
+	id: number
+	film_name: string
+	film_duration: number
+	film_description: string
+	film_origin: string
+	film_poster: string
+}
+
+export interface Hall {
+	id: number
+	hall_name: string
+	hall_rows: number
+	hall_places: number
+	hall_config: string[][]
+	hall_price_standart: number
+	hall_price_vip: number
+	hall_open: number
+}
